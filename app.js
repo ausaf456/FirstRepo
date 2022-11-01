ead96a3f2a381b740f191a5c72eb8aa8c49a7526
ausaf456 <ausafshah456@gmail.com>
2022-11-01 23:45:55 +0500

    done

---
+++ app.js
@@ -0,0 +1,63 @@
+// Chapter 1
+// alert("Hello World");
+
+
+
+// Chapter 2
+// var firststringvariable="Ausaf";
+// alert(firststringvariable)
+
+
+
+// Chapter 3
+// var age=2;
+// var agereturns=3;
+// alert(age+agereturns);
+
+
+
+// Chapter 4
+//Legal Names
+// var _Name="Ausaf";   
+// var Name="Ausaf";   
+// var name="Ausaf";   
+// var name2="Ausaf";   
+
+//Illegal Names
+// var 2name="Ausaf";
+// var name/="Ausaf";
+// var na&me="Ausaf";
+// var na me="Ausaf";
+
+
+
+
+
+// Chapter 5
+// var operator=3+3*2-1+2+2;
+// alert(operator);
+
+
+
+
+
+// Chapter 6
+// var jkl=2;
+// jkl--;
+// jkl++;
+// jkl++;
+// alert(jkl);
+
+
+
+
+//Chapter 7
+// var dfg=(2+2)+(3+3);
+// alert(dfg)
+
+
+
+// Chapter 8
+
+// var dsa = "pine" + "apple";
+// alert(dsa);
